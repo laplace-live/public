58df2a651740fc20592b9bcc6af0f8a570d91b0d
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2026-01-23 20:22:09 +0800

    Add events for 2026 week 4

--- data/events.tsx
+++ data/events.tsx
@@ -52,6 +52,14 @@ export const events: WeekItem[] = [
   //   { date: "2023-00-00 19:00+08:00", type: "", title: "", rec: "", },
   // ] },
 
+  { year: 2026, week: 4, events: [
+    { date: "2026-01-21 20:00+08:00", type: "watch", title: "高达0079", },
+    { date: "2026-01-22 20:00+08:00", type: "game", title: "真三国无双", },
+    { date: "2026-01-23 20:00+08:00", type: "game", title: "真三国无双", },
+    { date: "2026-01-24 19:00+08:00", type: "sub", title: "梦境之花", },
+    { date: "2026-01-25 19:00+08:00", type: "watch", title: "魔法晋书目录", },
+  ] },
+
   { year: 2026, week: 3, bilibili_url: "1157348997446238215", events: [
     { date: "2026-01-12 20:00+08:00", type: "rest", title: "", rec: "", },
     { date: "2026-01-13 20:00+08:00", type: "radio", title: "三国电台", rec: "", },
