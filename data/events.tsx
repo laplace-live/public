9bfeb2adf016c50cc6a8697b7ebdc8545ba7135e
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2026-04-15 15:38:21 +0800

    Add events for 2026 week 16

--- data/events.tsx
+++ data/events.tsx
@@ -52,6 +52,16 @@ export const events: WeekItem[] = [
   //   { date: "2023-00-00 19:00+08:00", type: "", title: "", rec: "", },
   // ] },
 
+  { year: 2026, week: 16, bilibili_url: "1191097725197746195", events: [
+    { date: "2026-04-13 20:00+08:00", type: "game", title: "哀鸿", },
+    { date: "2026-04-14 20:00+08:00", type: "rest", title: "", },
+    { date: "2026-04-15 20:00+08:00", type: "rest", title: "", },
+    { date: "2026-04-16 20:00+08:00", type: "game", title: "城破十日", },
+    { date: "2026-04-17 20:00+08:00", type: "game", title: "楚汉传奇", },
+    { date: "2026-04-18 19:00+08:00", type: "chat", title: "大家什么品味", },
+    { date: "2026-04-19 19:00+08:00", type: "radio", title: "电台TIME", },
+  ] },
+
   { year: 2026, week: 15, bilibili_url: "1188505124019896352", events: [
     { date: "2026-04-06 20:00+08:00", type: "game", title: "哀鸿", },
     { date: "2026-04-07 20:00+08:00", type: "rest", title: "", },
